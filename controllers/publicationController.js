d25c1fb74b72a2a8801faaf20582fc65ef052e4f
Valeri Arnaudov <99884520+valeriarnaudov@users.noreply.github.com>
2022-06-21 15:52:47 +0300

    Create page and logic

---
+++ controllers/publicationController.js
@@ -0,0 +1,19 @@
+const router = require("express").Router();
+const { isAuth } = require("../middlewares/authMiddleware");
+const publicationService = require("../services/publicationService");
+
+router.use(isAuth);
+
+router.get("/create", (req, res) => {
+    res.render("publication/create");
+});
+
+router.post("/create", async (req, res) => {
+    const publicationData = { ...req.body, author: req.user._id };
+
+    await publicationService.create(publicationData);
+
+    res.redirect("/publications");
+});
+
+module.exports = router;
